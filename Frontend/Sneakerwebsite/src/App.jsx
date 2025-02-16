9877155b71ce6e9135047b842f81d436b84d1def
Aayush Rai <arai03178@gmail.com>
2025-02-16 20:17:58 +0545

    Shop Category

--- Frontend/Sneakerwebsite/src/App.jsx
+++ Frontend/Sneakerwebsite/src/App.jsx
@@ -10,6 +10,9 @@ import Cart from './Pages/Cart';
 import Favourite from './Pages/Favourite';
 import LoginSignup from './Pages/LoginSignup';
 import Footer from './Component/Footer/Footer';
+import men_banner from './Component/assets/banner_mens.png'
+import women_banner from './Component/assets/banner_women.png'
+import kid_banner from './Component/assets/banner_kids.png'
 
 function App() {
   const [count, setCount] = useState(0);
@@ -20,9 +23,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Shop />} />
-          <Route path='/mens' element={<ShopCategory category="mens" />} />
-          <Route path='/womens' element={<ShopCategory category="womens" />} />
-          <Route path='/kids' element={<ShopCategory category="kids" />} />
+          <Route path='/mens' element={<ShopCategory banner={men_banner} category="men" />} />
+          <Route path='/womens' element={<ShopCategory banner={women_banner} category="women" />} />
+          <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid" />} />
           <Route path='/personalize' element={<Personalize />} />
           <Route path='/aboutUs' element={<AboutUs />} />
           <Route path="/product" element={<Product />} />
@@ -32,7 +35,6 @@ function App() {
           <Route path='/login' element={<LoginSignup />} />
         </Routes>
         <Footer />
-        
       </BrowserRouter>
     </div>
   );
